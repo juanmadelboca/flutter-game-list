af0af705840bdff653bfac7589031ef97d987cfa
Juanma Del boca <juanmadelboca@gmial.com>
2021-03-28 17:46:10 -0300

    Add list grouping

--- lib/widgets/game_card.dart
+++ lib/widgets/game_card.dart
@@ -10,116 +10,117 @@ class GameCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Card(
-              color: whiteCard,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              elevation: 3,
-              child: Container(
-                  height: 90,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: fromHex(game.homePrimaryColor),
-                              borderRadius: BorderRadius.only(
-                                topLeft: const Radius.circular(15.0),
-                                bottomLeft: const Radius.circular(15.0),
-                              ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+      child: Column(
+        children: [
+          Card(
+            color: whiteCard,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            elevation: 3,
+            child: Container(
+                height: 90,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: fromHex(game.homePrimaryColor),
+                            borderRadius: BorderRadius.only(
+                              topLeft: const Radius.circular(15.0),
+                              bottomLeft: const Radius.circular(15.0),
                             ),
-                            width: 10,
                           ),
-                          Container(
-                            color: fromHex(game.homeSecondaryColor),
-                            width: 10,
-                          ),
-                        ],
-                      ),
-                      Container(
-                        width: 100,
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                _calculateSpread(game.spread),
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.w400),
-                                textAlign: TextAlign.end,
-                              ),
-                              Text(game.homeTeam.teamName,
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
-                                  textAlign: TextAlign.end),
-                            ]),
-                      ),
-                      Text('vs'),
-                      Container(
-                        width: 100,
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                _calculateSpread(game.spread, awayTeam: true),
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.w400),
-                              ),
-                              Text(game.awayTeam.teamName,
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold)),
-                            ]),
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            color: fromHex(game.awayPrimaryColor),
-                            width: 10,
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: fromHex(game.awaySecondaryColor),
-                              borderRadius: new BorderRadius.only(
-                                topRight: const Radius.circular(15.0),
-                                bottomRight: const Radius.circular(15.0),
-                              ),
+                          width: 10,
+                        ),
+                        Container(
+                          color: fromHex(game.homeSecondaryColor),
+                          width: 10,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 100,
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              _calculateSpread(game.spread),
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w400),
+                              textAlign: TextAlign.end,
                             ),
-                            width: 10,
+                            Text(game.homeTeam.teamName,
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.end),
+                          ]),
+                    ),
+                    Text('vs'),
+                    Container(
+                      width: 100,
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              _calculateSpread(game.spread, awayTeam: true),
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w400),
+                            ),
+                            Text(game.awayTeam.teamName,
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold)),
+                          ]),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          color: fromHex(game.awayPrimaryColor),
+                          width: 10,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: fromHex(game.awaySecondaryColor),
+                            borderRadius: new BorderRadius.only(
+                              topRight: const Radius.circular(15.0),
+                              bottomRight: const Radius.circular(15.0),
+                            ),
                           ),
-                        ],
-                      ),
-                    ],
-                  )),
-            )),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal:10.0),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(children: [
-                Icon(getLeagueIcon(game.league)),
-                Text(leagueValues.reverse[game.league])
-              ],),
-              Row(children: [
-                Text(_friendlyDatePrint(game.gameDatetime)),
-                Icon(Icons.watch_later_outlined),
-              ],)
-            ],
+                          width: 10,
+                        ),
+                      ],
+                    ),
+                  ],
+                )),
           ),
-        )
-      ],
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0, right: 3.0, left: 3.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(children: [
+                  Icon(getLeagueIcon(game.league)),
+                  Text(leagueValues.reverse[game.league])
+                ],),
+                Row(children: [
+                  Text(_friendlyDatePrint(game.gameDatetime)),
+                  Icon(Icons.watch_later_outlined),
+                ],)
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 
