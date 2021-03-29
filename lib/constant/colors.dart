37538dc40f3f673f9f62c687b5e79bb6b21d5e5a
Juanma Del boca <juanmadelboca@gmial.com>
2021-03-29 03:02:05 -0300

    Add hooks

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const Color whiteCard = Color(0xFAFAFAFF);
+const Color wagrColor = Color(0xFFE8BD6F);
--- lib/repository/games.dart
+++ lib/repository/games.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
-import 'package:cloud_functions/cloud_functions.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:wagr_challenge/main.dart';
+import 'package:dio/dio.dart';
 import 'package:wagr_challenge/model/game.dart';
 import 'dart:async';
 
@@ -10,18 +6,11 @@ class GamesRepository {
   final cloudFunctionsTimeout = 60;
 
   Future<List<Game>> getGames() async {
-    //final HttpsCallableResult result = await cloudFunction('code-challenge-mobile-app-games');
-    //return result.data.map((e) => Game.fromJson(e)).where((e) => e != null).toList();
-
-    String data = await DefaultAssetBundle.of(globalContext).loadString('assets/response.json');
-    final result = json.decode(data);
-    return result.map<Game>((e) => Game.fromJson(Map<String, dynamic>.from(e))).toList();
-  }
-
-  Future cloudFunction(String name, {Map<String, dynamic> params}) async {
-    final HttpsCallable callable = FirebaseFunctions.instance.httpsCallable(name,
-        options: HttpsCallableOptions(
-            timeout: Duration(seconds: cloudFunctionsTimeout)));
-    return await callable.call(params ?? Map<String, dynamic>());
+    var response = await Dio().get('https://us-central1-wagr-develop.cloudfunctions.net/code-challenge-mobile-app-games');
+    if(response.statusCode == 200) {
+      return response.data.map<Game>((e) => Game.fromJson(Map<String, dynamic>.from(e))).toList();
+    } else {
+      return List<Game>();
+    }
   }
 }
