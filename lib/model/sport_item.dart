00f8c537757dc0a7d3476773fa4c8a3cc41ff350
Del Boca Juan Manuel <juanmadelboca@gmail.com>
2021-03-28 23:40:42 -0300

    Add filter by sport (#3)
    Co-authored-by: Juanma Del boca <juanmadelboca@gmial.com>

---
+++ lib/model/sport_item.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class SportItem{
+  final IconData icon;
+  final String displayName;
+
+  SportItem(this.icon, this.displayName);
+}
--- lib/view/games_screen.dart
+++ lib/view/games_screen.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 import 'package:wagr_challenge/model/date_filter.dart';
 import 'package:wagr_challenge/model/game.dart';
+import 'package:wagr_challenge/model/sport_item.dart';
 import 'package:wagr_challenge/repository/games.dart';
 import 'package:wagr_challenge/util/date.dart';
 import 'package:wagr_challenge/widgets/filter_tab.dart';
@@ -19,11 +20,11 @@ class _GamesScreenState extends State<GamesScreen> {
   int selected = 0;
   ItemScrollController scrollController = ItemScrollController();
   List<DateFilter> _filters;
+  Sport sportFilter = Sport.ALL;
 
   @override
   Widget build(BuildContext context) {
     GamesRepository gamesRepository = Provider.of(context);
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: FutureBuilder<Object>(
@@ -37,7 +38,10 @@ class _GamesScreenState extends State<GamesScreen> {
                   child: CircularProgressIndicator(),
                 ),
               );
-            final List<Game> games = snapshot.data;
+            List<Game> games = snapshot.data;
+            if (sportFilter != Sport.ALL) {
+              games = games.where((element) => (element.sport == sportFilter)).toList();
+            }
             _filters = _getFilterList(games.first.gameDatetime);
             return Column(
               children: [
@@ -49,10 +53,37 @@ class _GamesScreenState extends State<GamesScreen> {
                     padding: const EdgeInsets.symmetric(horizontal: 15),
                     child: Container(
                       width: double.infinity,
-                      child: Text(
-                        'Games',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(fontSize: 28, fontWeight: FontWeight.w400),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Games',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(fontSize: 28, fontWeight: FontWeight.w400),
+                          ),
+                          DropdownButton<Sport>(
+                              value: sportFilter,
+                              underline: SizedBox(),
+                              iconSize: 42,
+                              onChanged: (Sport newValue) {
+                                setState(() {
+                                  sportFilter = newValue;
+                                });
+                              },
+                              items: Sport.values.map((Sport sport) {
+                                SportItem sportItem = _getSportItem(sport);
+                                return DropdownMenuItem<Sport>(
+                                    value: sport,
+                                    child: Row(
+                                      children: [
+                                        Icon(sportItem.icon),
+                                        SizedBox(width: 5,),
+                                        Text(sportItem.displayName),
+                                      ],
+                                    ));
+                              }).toList()
+                          )
+                        ],
                       ),
                     ),
                   ),
@@ -74,7 +105,7 @@ class _GamesScreenState extends State<GamesScreen> {
                             selected = index;
                           });
                           int scrollIndex = games.indexWhere(
-                              (element) => compareGameDate(element.gameDatetime, _filters[index].dateTime));
+                                  (element) => compareGameDate(element.gameDatetime, _filters[index].dateTime));
                           scrollController.scrollTo(index: scrollIndex, duration: Duration(milliseconds: 800));
                         },
                       );
@@ -83,34 +114,34 @@ class _GamesScreenState extends State<GamesScreen> {
                 ),
                 games != null
                     ? Expanded(
-                        child: ScrollablePositionedList.builder(
-                            itemScrollController: scrollController,
-                            itemCount: games?.length ?? 0,
-                            itemBuilder: (context, index) {
-                              return Column(
-                                children: [
-                                  if (index == 0 ||
-                                      index == games.length ||
-                                      !compareGameDate(games[index].gameDatetime, games[index - 1].gameDatetime))
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
-                                      child: Container(
-                                        child: Text(
-                                          _filters
-                                              .firstWhere((element) =>
-                                                  compareGameDate(element.dateTime, games[index].gameDatetime))
-                                              .displayName,
-                                          textAlign: TextAlign.start,
-                                          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 22),
-                                        ),
-                                        width: double.maxFinite,
-                                      ),
-                                    ),
-                                  GameCard(games[index]),
-                                ],
-                              );
-                            }),
-                      )
+                  child: ScrollablePositionedList.builder(
+                      itemScrollController: scrollController,
+                      itemCount: games?.length ?? 0,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            if (index == 0 ||
+                                index == games.length ||
+                                !compareGameDate(games[index].gameDatetime, games[index - 1].gameDatetime))
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
+                                child: Container(
+                                  child: Text(
+                                    _filters
+                                        .firstWhere((element) =>
+                                        compareGameDate(element.dateTime, games[index].gameDatetime))
+                                        .displayName,
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 22),
+                                  ),
+                                  width: double.maxFinite,
+                                ),
+                              ),
+                            GameCard(games[index]),
+                          ],
+                        );
+                      }),
+                )
                     : Container(child: Center(child: Text('Error')))
               ],
             );
@@ -121,55 +152,27 @@ class _GamesScreenState extends State<GamesScreen> {
   List<DateFilter> _getFilterList(DateTime dateTime) {
     List<DateFilter> filters = List<DateFilter>();
     final now = dateTime;
-    filters.add(DateFilter(now, 'Today'));
-    filters.add(DateFilter(DateTime(now.year, now.month, now.day + 1), 'Tomorrow'));
+    filters.add(DateFilter(now, displayName: 'Today'));
+    filters.add(DateFilter(DateTime(now.year, now.month, now.day + 1), displayName: 'Tomorrow'));
     for (int i = 2; i < 8; i++) {
       final date = DateTime(now.year, now.month, now.day + i);
-      filters.add(DateFilter(date, _getFilterName(date)));
-    }
-    return filters;
-  }
+      filters.add(DateFilter(date));
+      }
+          return filters;
+      }
 
-  String _getFilterName(DateTime dateTime) {
-    String month;
-    switch (dateTime.month) {
-      case 1:
-        month = 'Jan';
-        break;
-      case 2:
-        month = 'Feb';
-        break;
-      case 3:
-        month = 'Mar';
-        break;
-      case 4:
-        month = 'Apr';
-        break;
-      case 5:
-        month = 'May';
-        break;
-      case 6:
-        month = 'Jun';
-        break;
-      case 7:
-        month = 'Jul';
-        break;
-      case 8:
-        month = 'Aug';
-        break;
-      case 9:
-        month = 'Sep';
-        break;
-      case 10:
-        month = 'Oct';
-        break;
-      case 11:
-        month = 'Nov';
-        break;
-      case 12:
-        month = 'Dec';
-        break;
+  SportItem _getSportItem(Sport sport) {
+    switch (sport) {
+      case Sport.SOCCER:
+        return SportItem(Icons.sports_soccer, 'Soccer');
+      case Sport.FOOTBALL:
+        return SportItem(Icons.sports_football_outlined, 'Football');
+      case Sport.BASKETBALL:
+        return SportItem(Icons.sports_basketball, 'Basketball');
+      case Sport.ICE_HOCKEY:
+        return SportItem(Icons.sports_hockey, 'Ice Hockey');
+      default:
+        return SportItem(Icons.sports_handball, 'All Sports');
     }
-    return month + ' ' + dateTime.day.toString();
   }
 }
